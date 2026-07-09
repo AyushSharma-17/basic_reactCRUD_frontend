192357f071cd791f34d6e4c05447feab9ac8e61d
ayush <ayushsharma12345qwerty@gmail.com>
2026-07-09 23:45:40 +0530

    fetching all the method of HTTP

--- src/studentService.js
+++ src/studentService.js
@@ -5,3 +5,11 @@ const API_URL = 'http://localhost:8080/students';
 //get all students
 export const getStudents=()=>axios.get(API_URL);
 
+//POST new student
+export const addStudent=(student)=>axios.post(API_URL,student);
+
+//PUT update student
+export const updateStudent=(id,student)=>axios.put(`${API_URL}/${id}`,student);
+
+//DELETE student
+export const deleteStudent=(id)=>axios.delete(`${API_URL}/${id}`);
